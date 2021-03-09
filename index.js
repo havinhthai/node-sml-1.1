6e069ff1ee136373796c124968997e9a223f010c
Ha Vinh Thai <thaihv@Has-MacBook-Pro.local>
2021-03-09 21:34:35 +0700

    Add Express (done)

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('> Hi')
+const express = require('express');
+
+const app = express();
+
+// callback
+
+app.get('/home', (req, res) => {
+    res.send('<a href="/chuyen-muc">Go to category page</a> <br />');
+});
+
+app.get('/chuyen-muc', (req, res) => {
+    res.send('<a href="/home">Back to Home!</a>')
+});
+
+app.get('/chuyen-muc/:name/:age', (req, res) => {
+    const name = req.params.name;
+    const age = req.params.age;
+    
+    res.send(postId);
+})
+
+app.listen(3002, () => {
+    console.log('> Running');
+})
