acac308d4b52e91664ca8dcc4d15517d10453b7b
Ha Vinh Thai <thaihv@Has-MacBook-Pro.local>
2021-03-13 19:25:44 +0700

    Add static folder (done)

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-// callback
+// Setting view engine for Express
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Setting static folder
+app.use('/public', express.static('./public'))
 
 app.get('/home', (req, res) => {
     res.send('<a href="/chuyen-muc">Go to category page</a> <br />');
@@ -12,13 +18,6 @@ app.get('/chuyen-muc', (req, res) => {
     res.send('<a href="/home">Back to Home!</a>')
 });
 
-app.get('/chuyen-muc/:name/:age', (req, res) => {
-    const name = req.params.name;
-    const age = req.params.age;
-    
-    res.send(postId);
-})
-
-app.listen(3002, () => {
+app.listen(3000, () => {
     console.log('> Running');
 })
