515f492c76c60e64ee481e26544977824a426fbb
Ha Vinh Thai <thaihv@Has-MacBook-Pro.local>
2021-03-16 21:04:08 +0700

    Modulize routes (done)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const ejsEngine = require('ejs-mate');
 
+// Lấy đối tượng router từ file ./routes/index.js
+const routes = require('./routes');
+
 const app = express();
 
 // Setting view engine for Express
@@ -13,50 +16,7 @@ app.engine('ejs', ejsEngine);
 app.use('/public', express.static('./public'));
 
 // Routes
-app.get('/', (req, res) => {
-    // Tạo biến postsTao trên server
-    const postsTao = [
-        { 
-            title: 'Tin giật gân 1', 
-            description: 'mo tar', 
-            author: 'Thai', 
-            createdAt: '13/02/2021' 
-        },
-        {
-            title: 'Tin giật gân 2',
-            description: 'mo tar',
-            author: 'Thai',
-            createdAt: '13/02/2021'
-        },
-        {
-            title: 'Tin giật gân 3',
-            description: 'mo tar',
-            author: 'Thai',
-            createdAt: '13/02/2021'
-        }
-    ];
-
-    res.render('client/page/homepage', {
-        posts: postsTao, // Truyền biến posts từ Server xuống view qua hàm render
-    });
-});
-
-app.get('/post', (req, res) => {
-    // Bước 1: Xác định dữ liệu truyền xuống View
-    // title, description, content, author, 
-    const post = {
-        title: 'ABC',
-        description: 'DDaya la motaaa',
-        content: '2uawdahsdjladdd....',
-        author: 'mr T',
-        authorBlog: 'https://tuidev.io',
-        createdAt: '15/03/2021'
-    };
-
-    res.render('client/page/post', {
-        post: post, // Bước 2: Truyền dữ liệu từ Express xuống View
-    });
-})
+app.use(routes); // Áp dụng routes đã import vào dự án
 
 app.listen(3000, () => {
     console.log('> Running');
