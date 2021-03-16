b418a3dc893ff41345e53870335bae3a59a30ea0
Ha Vinh Thai <thaihv@Has-MacBook-Pro.local>
2021-03-16 20:39:17 +0700

    Send data from server to view  (done)

--- index.js
+++ index.js
@@ -14,12 +14,48 @@ app.use('/public', express.static('./public'));
 
 // Routes
 app.get('/', (req, res) => {
-    // res.send('<a href="/chuyen-muc">Go to category page</a> <br />');
-    res.render('client/page/homepage');
+    // Tạo biến postsTao trên server
+    const postsTao = [
+        { 
+            title: 'Tin giật gân 1', 
+            description: 'mo tar', 
+            author: 'Thai', 
+            createdAt: '13/02/2021' 
+        },
+        {
+            title: 'Tin giật gân 2',
+            description: 'mo tar',
+            author: 'Thai',
+            createdAt: '13/02/2021'
+        },
+        {
+            title: 'Tin giật gân 3',
+            description: 'mo tar',
+            author: 'Thai',
+            createdAt: '13/02/2021'
+        }
+    ];
+
+    res.render('client/page/homepage', {
+        posts: postsTao, // Truyền biến posts từ Server xuống view qua hàm render
+    });
 });
 
 app.get('/post', (req, res) => {
-    res.render('client/page/post');
+    // Bước 1: Xác định dữ liệu truyền xuống View
+    // title, description, content, author, 
+    const post = {
+        title: 'ABC',
+        description: 'DDaya la motaaa',
+        content: '2uawdahsdjladdd....',
+        author: 'mr T',
+        authorBlog: 'https://tuidev.io',
+        createdAt: '15/03/2021'
+    };
+
+    res.render('client/page/post', {
+        post: post, // Bước 2: Truyền dữ liệu từ Express xuống View
+    });
 })
 
 app.listen(3000, () => {
